b7fb5ed15fcc2f7327466997ef78a38d97821502
YJ2CS <cnyjzhang@outlook.com>
2020-11-28 15:02:44 +0800

    - add changer .gitignore about themes folder - change foam settings.json - change README.md - to ADD  出现bug的自我检查修复 可选功能 - change 修改一些脚本细节 - add 增加大部分的安装和配置说明 - ADD LICENSE

--- scripts/auto_backup.js
+++ scripts/auto_backup.js
@@ -26,7 +26,8 @@ function run() {
 
         echo("======================Auto Backup Begin===========================");
 
-        cd('E:/work/CS-BlogSource');    //此处修改为Hexo根目录路径
+        //使用idea，此行命令注释掉
+        //cd('E:/work/CS-BlogSource');    //此处修改为Hexo根目录路径
 
         if (exec('git add --all').code !== 0) {
 
@@ -44,9 +45,14 @@ function run() {
 
         }
 
-        if (exec('git push origin master').code !== 0) {
+        if (exec('git push origin main').code !== 0) {
+        //提交本地main分支作为远程的main分支
+            // git push origin main
+        // 提交本地dev分支作为远程的master分  好像只写这一句，远程的github就会自动创建一个dev分支
+        // 注意，这个操作十分危险，它会完全覆盖远程的master分支，其他提交者所作的更改会丢失
+            // git push origin dev:master
 
-            echo('Error: Git push failed');
+            echo('Error: Git push origin main failed');
 
             exit(1);
 
